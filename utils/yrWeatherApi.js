0b3f11678ff32c05306fc709882c8b4786bb9a12
jcvic1 <victorjcdon@gmail.com>
2024-05-16 03:50:13 +0300

    interpolation added

--- utils/yrWeatherApi.js
+++ utils/yrWeatherApi.js
@@ -13,17 +13,49 @@ const getWeatherData = async (long = 55.7522, lat = 37.6155) => {
             }
         });
 
-        const weatherData = response.data.properties.timeseries.filter(entry => {
-            const date = entry.time.split('T')[1].split(":")[0];
-            return parseInt(date, 10) === 14;
-        });
+        const timeData = response.data.properties.timeseries;
 
-        const temperaturesByDay = weatherData.map(entry => {
+
+        const exactDate = timeData.filter(entry => {
+            const date = new Date(entry.time);
+            return date.getUTCHours() === 14;
+        });
+        const temperaturesByDay = exactDate.map(entry => {
             return {
                 date: entry.time,
                 temperature: entry.data.instant.details.air_temperature
             };
         });
+
+        const interpolateArrayData = timeData.filter(entry => {
+            const date = new Date(entry.time);
+            if ((entry.time > exactDate[exactDate.length - 1].time) && (date.getUTCHours() === 12 || date.getUTCHours() === 18)) {
+
+                return entry;
+            }
+        });
+
+        for (let i = 1; i < interpolateArrayData.length - 1; i++) {
+            const prev = interpolateArrayData[i];
+            const next = interpolateArrayData[i + 1];
+            const prevHour = new Date(prev.time).getUTCHours();
+            const prevTemp = prev.data.instant.details.air_temperature;
+            const nextHour = new Date(next.time).getUTCHours();
+            const nextTemp = next.data.instant.details.air_temperature;
+
+            const dateTime = new Date(prev.time);
+            dateTime.setUTCHours(dateTime.getUTCHours() + 2);
+
+            const temp14 = prevTemp + (nextTemp - prevTemp) * (14 - prevHour) / (nextHour - prevHour)
+
+            temperaturesByDay.push({
+                date: dateTime.toISOString().slice(0, 19) + 'Z',
+                temperature: parseFloat(temp14.toFixed(1))
+            })
+
+            i++;
+
+        }
 
         return { message: "success", result: temperaturesByDay };
     } catch (error) {
