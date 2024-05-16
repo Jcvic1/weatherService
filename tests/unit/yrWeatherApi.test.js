57c1f7646711762f8a7711817febf1116302c249
jcvic1 <victorjcdon@gmail.com>
2024-05-16 04:25:51 +0300

    test on interpolation

--- tests/unit/yrWeatherApi.test.js
+++ tests/unit/yrWeatherApi.test.js
@@ -18,7 +18,37 @@ describe('getWeatherData', () => {
                   }
                 }
               }
-            }
+            },
+            {
+              time: "2024-05-09T18:00:00Z",
+              data: {
+                instant: {
+                  details: {
+                    air_temperature: 24.5
+                  }
+                }
+              }
+            },
+            {
+              time: "2024-05-10T12:00:00Z",
+              data: {
+                instant: {
+                  details: {
+                    air_temperature: 30.0
+                  }
+                }
+              }
+            },
+            {
+              time: "2024-05-10T18:00:00Z",
+              data: {
+                instant: {
+                  details: {
+                    air_temperature: 36.6
+                  }
+                }
+              }
+            },
           ]
         }
       }
@@ -28,7 +58,7 @@ describe('getWeatherData', () => {
 
     const result = await getWeatherData(37.6155, 55.7522);
 
-    expect(result).toEqual({ message: 'success', result: [{ date: "2024-05-09T14:00:00Z", temperature: 20.5 }] });
+    expect(result).toEqual({ message: 'success', result: [{ date: "2024-05-09T14:00:00Z", temperature: 20.5 }, { date: "2024-05-10T14:00:00Z", temperature: 32.2 }] });
     expect(axios.get).toHaveBeenCalledWith(
       'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=55.7522&lon=37.6155',
       expect.objectContaining({
